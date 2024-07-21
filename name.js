435156dcef3739e7e03f7b8bc5eabc98f823c99d
aganepaul <paulagane94@gmail.com>
2024-07-21 09:16:45 +0100

    third commit

---
+++ name.js
@@ -0,0 +1,21 @@
+var name=prompt('What is your name?')
+alert(' Hello '+name+' you are welcome ')
+
+var age=prompt('How old are you')
+alert('surprise! you are my age mate. what a co-incidence')
+
+var gender=prompt('What gender')
+alert('my opposite sex,nice meeting you')
+
+var country=prompt('which country are you from')
+alert(country+'! the country of my dream ')
+
+var Class=prompt('What class are you')
+alert('I gap you with just a class, I am your senior in school')
+
+var request=prompt(' Can I ask you for one request')
+alert('hope you will not turn me down')
+
+var friends=prompt('Can we be friends')
+alert('wow! I am so happy right now, I now have a friend in your country. Thank you so much. Chat you later. Bye for now') 
+undefined
